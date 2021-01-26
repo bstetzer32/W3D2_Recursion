4b4fbdf98f495443b5d131009995ae3803af29a9
James Sullivan <jsullivan86@gmail.com>
2021-01-26 14:42:51 -0800

    We tried our best

--- recursion-problems-starter/problems/11-flatten.js
+++ recursion-problems-starter/problems/11-flatten.js
@@ -10,8 +10,18 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
-// your code here
-  
+function flatten(array) {
+  if (array.length === 0) {
+    return [];
+  }
+  if (typeof array[array.length - 1] === 'object') {
+    array = [[array.slice(0, array.length - 1)], ...array[array.length - 1]];
+    return flatten(array);
+  }
+  return array;
+}
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = flatten;
