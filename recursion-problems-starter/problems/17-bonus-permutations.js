0e924d19e585aa65a8970915bad9035a5617eef8
Ben Stetzer <benstetzer@gmail.com>
2021-01-27 16:58:02 -0500

    bonus permutations

--- recursion-problems-starter/problems/17-bonus-permutations.js
+++ recursion-problems-starter/problems/17-bonus-permutations.js
@@ -11,9 +11,34 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
                         // [2, 1, 3], [2, 3, 1],
                         // [3, 1, 2], [3, 2, 1]]
 ***********************************************************************/
+function factorial(num) {
+  if (num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
 
-// your code here
+function permutations(array) {
+  let newArray = [array.slice(0)]
+  if (newArray.length === factorial(array.length)) {
+    debugger
+    return array;
+  }
 
+  for (let i = 0; i < array.length -; i++) {
+    let el1 = array[i];
+    let el2 = array[i + 1];
+    let nEl1 = newArray[i];
+    let nEl2 = newArray[i + 1];
+    if (el1 === nEl1 && el2 === nEl2) {
+      newArray[i] = nEl2;
+      newArray[i + 1] = nEl1;
+    }
+  }
+
+  return [array, permutations(newArray)]
+}
+console.log(permutations([1, 2, 3])) // [[1, 2], [2, 1]]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = permutations;
